05187b20917288ab305ceb4b47895aea3596b229
AJN <buzz781@gmail.com>
2023-11-10 14:56:32 +0900

    feat: order 클래스 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,5 +1,24 @@
 package christmas.domain.order;
 
-public class Order {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Order {
+	private int visitDay;
+	
+	private Map<String,Integer> orderMenu = new HashMap<>();
+	
+	private int orderMoney;
+	
+	private boolean isGiftIncluded;
+	
+	private Map<String,Integer> rewardsList = new HashMap<>();
+	
+	Order(int visitDay){
+		this.visitDay = visitDay;
+		this.rewardsList.put("크리스마스 디데이 할인:", 0);
+		this.rewardsList.put("평일 할인:", 0);
+		this.rewardsList.put("특별 할인:", 0);
+		this.rewardsList.put("증정 이벤트:", 0);
+	}
 }
