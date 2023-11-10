16200f49959264f5e94eed95b8a6244409f1a023
AJN <buzz781@gmail.com>
2023-11-10 14:11:54 +0900

    feat: 날짜 입력받기 구현 및 유효성검사(숫자유무) 구현

--- src/main/java/christmas/util/Validate.java
+++ src/main/java/christmas/util/Validate.java
@@ -1,5 +1,11 @@
 package christmas.util;
 
 public class Validate {
-
+	private static final String NOT_NUMBER = "[ERROR] 숫자만 입력해주세요.";
+	
+	public static void checkNumber(String inputData) {
+		for(char word : inputData.toCharArray()) {
+			if(!Character.isDigit(word)) throw new  IllegalArgumentException(inputData);
+		}
+	}
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,5 +1,19 @@
 package christmas.view;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
+import christmas.util.Validate;
 
+public class InputView {
+	public int promptForVisitDate() {
+	    System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+	    do {
+	    	String inputData = Console.readLine();
+	    	try {
+	    		Validate.checkNumber(inputData);
+				return Integer.parseInt(inputData);
+			} catch (IllegalArgumentException error) {
+				System.out.println(error.getMessage());
+			}
+	    }while(true);
+	}
 }
