ff150d4daa65fdd189359923c25345370eb8e5a9
AJN <buzz781@gmail.com>
2023-11-10 16:01:58 +0900

    feat: 주문안내 메시지 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,4 +4,7 @@ public class OutputView {
 	public static void printWelcomeMessage() {
 		System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
 	}
+	public static void printOrderMenuInformation() {
+		System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+	}
 }
