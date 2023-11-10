22e68dd02fb78ad156fef5f494c073e31f5b4789
AJN <buzz781@gmail.com>
2023-11-10 15:03:01 +0900

    docs&feat: 기능구현목록 문서 수정 및 order 클래스 게터세터 추가, 생성자 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -21,4 +21,46 @@ public class Order {
 		this.rewardsList.put("특별 할인:", 0);
 		this.rewardsList.put("증정 이벤트:", 0);
 	}
+
+	public int getVisitDay() {
+		return visitDay;
+	}
+
+	public void setVisitDay(int visitDay) {
+		this.visitDay = visitDay;
+	}
+
+	public Map<String, Integer> getOrderMenu() {
+		return orderMenu;
+	}
+
+	public void setOrderMenu(Map<String, Integer> orderMenu) {
+		this.orderMenu = orderMenu;
+	}
+
+	public int getOrderMoney() {
+		return orderMoney;
+	}
+
+	public void setOrderMoney(int orderMoney) {
+		this.orderMoney = orderMoney;
+	}
+
+	public boolean isGiftIncluded() {
+		return isGiftIncluded;
+	}
+
+	public void setGiftIncluded(boolean isGiftIncluded) {
+		this.isGiftIncluded = isGiftIncluded;
+	}
+
+	public Map<String, Integer> getRewardsList() {
+		return rewardsList;
+	}
+
+	public void setRewardsList(Map<String, Integer> rewardsList) {
+		this.rewardsList = rewardsList;
+	}
+	
+	
 }
