a2a0258206b4bae13713af96571d7d541033c103
AJN <buzz781@gmail.com>
2023-11-10 16:32:28 +0900

    feat: order 생성하는 컨트롤러 메소드 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,9 +1,23 @@
 package christmas.controller;
 
+import christmas.domain.order.Order;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class ChristmasController {
-	//TODO: createPlayerOrder()
+	private static Order createPlayerOrder() {
+		OutputView.printWelcomeMessage();
+		Order order = new Order(InputView.promptForVisitDate());
+		return order;
+	}
+	private static void playerOrderItem(Order order) {
+		OutputView.printOrderMenuInformation();
+		
+	}
+	
 	
 	void run() {
+		Order order = createPlayerOrder();
 		
 	}
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.util.Validate;
 
 public class InputView {
-	public int promptForVisitDate() {
+	public static int promptForVisitDate() {
 	    System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
 	    do {
 	    	String inputData = Console.readLine();
@@ -17,4 +17,6 @@ public class InputView {
 			}
 	    }while(true);
 	}
+	
+	
 }
