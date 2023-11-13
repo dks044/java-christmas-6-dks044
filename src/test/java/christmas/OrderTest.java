a8b8347e12bdeb445aa929f79079166b1594ac41
AJN <buzz781@gmail.com>
2023-11-13 17:44:59 +0900

    test: 5개 할인이벤트 메소드 테스트코드 구현

--- src/test/java/christmas/OrderTest.java
+++ src/test/java/christmas/OrderTest.java
@@ -66,20 +66,20 @@ public class OrderTest {
 	    assertEquals(testCollectData, order.getRewardsList().get("크리스마스 디데이 할인:"));
 	}
 	
-//	@DisplayName("5가지의 할인이벤트가 총 적용되는지 검사한다")
-//	@Test
-//	void 할인이벤트전체테스트() {
-//		Order order = new Order(3);
-//		String inputTestData = Util.parseInputDataToEngName("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
-//		OrderService.appendOrderMenu(order, inputTestData);
-//		order.setOrderMoney(OrderService.getTotalOrderMoney(order));
-//		OrderService.totalDiscountEvent(order);
-//		assertEquals(1200, order.getRewardsList().get("크리스마스 디데이 할인:"),"3일이니 1200원 할인되어야 합니다.");
-//		assertEquals(4046, order.getRewardsList().get("평일 할인:"),"디저트메뉴는 2개니까, 4046원 할인되어야 합니다.");
-//		assertEquals(0, order.getRewardsList().get("주말 할인:"),"주말이 아니므로 0원이어야 합니다.");
-//		assertEquals(1000, order.getRewardsList().get("특별 할인:"),"이벤트 달력에 별이 있는날이므로, 1000원 할인되어야 합니다.");
-//		assertEquals(25000, order.getRewardsList().get("증정 이벤트:"),"12만원 이상 주문했으므로, 25000원치 혜택이 적용되어야 합니다.");
-//	}
+	@DisplayName("5가지의 할인이벤트가 총 적용되는지 검사한다")
+	@Test
+	void 할인이벤트전체테스트() {
+		Order order = new Order(3);
+		String inputTestData = Util.parseInputDataToEngName("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+		OrderService.appendOrderMenu(order, inputTestData);
+		order.setOrderMoney(OrderService.getTotalOrderMoney(order));
+		OrderService.totalDiscountEvent(order);
+		assertEquals(1200, order.getRewardsList().get("크리스마스 디데이 할인:"),"3일이니 1200원 할인되어야 합니다.");
+		assertEquals(4046, order.getRewardsList().get("평일 할인:"),"디저트메뉴는 2개니까, 4046원 할인되어야 합니다.");
+		assertEquals(0, order.getRewardsList().get("주말 할인:"),"주말이 아니므로 0원이어야 합니다.");
+		assertEquals(1000, order.getRewardsList().get("특별 할인:"),"이벤트 달력에 별이 있는날이므로, 1000원 할인되어야 합니다.");
+		assertEquals(25000, order.getRewardsList().get("증정 이벤트:"),"12만원 이상 주문했으므로, 25000원치 혜택이 적용되어야 합니다.");
+	}
 	
 	
 	
