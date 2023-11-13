e99eb2c60d8307adc19875f5bc28497b64bcfc8a
AJN <buzz781@gmail.com>
2023-11-13 17:44:08 +0900

    fix: 평일 주말 로직 수정

--- src/main/java/christmas/domain/order/OrderService.java
+++ src/main/java/christmas/domain/order/OrderService.java
@@ -16,8 +16,8 @@ public class OrderService {
 	private static final List<MenuEnumInterface> allMenuItems = new ArrayList<>();
 	private static final int CHRISTMAS_DAY_EVENT_START = 1;
 	private static final int CHRISTMAS_DAY_EVENT_END = 25;
-	private static final List<Integer> WEEKDAY = List.of(1,4,5,6,7,8,11,12,13,14,15,18,19,20,21,22,25,26,27,28,29);
-	private static final List<Integer> WEEKEND = List.of(2,3,9,10,16,17,23,24,31);
+	private static final List<Integer> WEEKDAY = List.of(3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31);
+	private static final List<Integer> WEEKEND = List.of(1,2,8,9,15,16,22,23,29,30);
 	private static final List<Integer> SPECIAL_DAY = List.of(3,10,17,24,25,31); 
 	private static final List<String> DESSERT_ITEM_LIST = List.of("CHOCOLATE_CAKE","ICE_CREAM");
 	private static final List<String> MAIN_ITEM_LIST = List.of("T_BONE_STEAK","BBQ_RIBS","SEAFOOD_PASTA","CHRISTMAS_PASTA");
@@ -110,7 +110,7 @@ public class OrderService {
 		}
 		order.setRewardsList(rewardList);
 	}
-	
+		
 	public static void totalDiscountEvent(Order order) {
 		if(order.getOrderMoney()>=MINIMUM_PURCHASE_FOR_DISCOUNT) {
 			OrderService.chrismasDayEvent(order);
